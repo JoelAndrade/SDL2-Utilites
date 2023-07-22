aecdb8a340d77fba8ca89c99f7583a3ea6164998
Joel <joelandrade565@yahoo.com>
2023-07-22 10:27:03 -0700

    Changed the format

--- SDL2/SDL_Util.cpp
+++ SDL2/SDL_Util.cpp
@@ -13,13 +13,16 @@ SDL_Color white =  {255, 255, 255};
 SDL_Color orange = {255, 100,   0};
 SDL_Color yellow = {255, 255,   0};
 
-void frameCap(int fps, Uint32 startingTick) {
-    if (1000/fps > SDL_GetTicks() - startingTick) {
+void frameCap(int fps, Uint32 startingTick)
+{
+    if (1000/fps > SDL_GetTicks() - startingTick)
+    {
         SDL_Delay(1000/fps - (SDL_GetTicks() - startingTick));
     }
 }
 
-void updateCursorPos(SDL_Rect* rect, int xPos, int yPos) {
+void updateCursorPos(SDL_Rect* rect, int xPos, int yPos)
+{
     rect->x = xPos;
     rect->y = yPos;
 }
